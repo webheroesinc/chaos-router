2fe708612abc7ece1a3879a5812e082c9795cfee
Matthew Brisebois <matthew@webheroes.ca>
2015-09-11 21:54:36 +0000

    Published npm version 0.1.3

--- nodejs/chaosrouter.js
+++ nodejs/chaosrouter.js
@@ -1,4 +1,5 @@
 
+var Promise	= require('promise');
 var restruct	= require('restruct-data');
 var fs		= require('fs');
 
@@ -40,12 +41,12 @@ ChaosRouter.prototype.set_db	= function(db) {
 }
 ChaosRouter.prototype.extend_methods	= function(dict) {
     dict.iteritems(function(k,v) {
-	methodlib.prototype[k] = v;
+	methodlib[k] = v;
     });
 }
 ChaosRouter.prototype.extend_validation	= function(dict) {
     dict.iteritems(function(k,v) {
-	validationlib.prototype[k] = v;
+	validationlib[k] = v;
     });
 }
 ChaosRouter.prototype.route	= function(path, data, parents) {
@@ -68,7 +69,8 @@ ChaosRouter.prototype.route	= function(path, data, parents) {
     if (!path)
 	return Endpoint(this.config, variables, parents, this.db);
 
-    segs.iterate(function(seg) {
+    for (var i in segs) {
+	var seg		= segs[i];
 	if (seg === "..") {
 	    data	= parents.pop()[1];
 	    return;
@@ -91,7 +93,7 @@ ChaosRouter.prototype.route	= function(path, data, parents) {
 	else
 	    data	= data.Get(seg);
 	parents.append([seg,data]);
-    });
+    }
     parents.pop();
 
     if (data.Get('.base') === null)
@@ -129,6 +131,8 @@ function Endpoint(config, path_vars, parents, db) {
     this.db		= setdefault(db, null);
 }
 Endpoint.prototype.set_arguments	= function(args) {
+    if (!is_iterable(args))
+	return false;
     ['db', 'value', 'path'].iterate(function(reserved) {
 	if (reserved.in(args))
 	    delete args[reserved];
@@ -136,7 +140,11 @@ Endpoint.prototype.set_arguments	= function(args) {
     this.args.update(args);
 }
 Endpoint.prototype.validate	= function(validations) {
-    var $this		= this;
+    if (validations === null || validations === undefined)
+    	return Promise.resolve();
+    
+    var self		= this;
+    var promises	= [];
     validations.iterate(function(rule) {
 	if (! is_list(rule) || len(rule) === 0)
 	    throw new Error("Failed to process rule: {0}".format(rule));
@@ -144,7 +152,7 @@ Endpoint.prototype.validate	= function(validations) {
 	var params	= [];
 	rule.slice(1).iterate(function(param) {
 	    try {
-		var value	= fill(param, $this.args);
+		var value	= fill(param, self.args);
 	    }
 	    catch (e) {
 		var value	= null;
@@ -154,68 +162,22 @@ Endpoint.prototype.validate	= function(validations) {
 	var cmd		= validationlib.Get(command);
 	if (cmd === null)
 	    throw new Error("No validation method for rule {0}".format(rule));
-	var check	= cmd(params, $this.args, $this.db);
-
-	if (is_dict(check))
-	    return check;
-	if (check !== true) {
-	    var message	= "Failed at rule {0} with values {1}".format(rule, params);
-	    return {
-		"error": "Failed Validation",
-		"message": is_string(check) ? check : message
-	    }
-	}
+	promises.append(new Promise(function(f,r){
+	    cmd.call(validationlib, function(check) {
+		if (is_dict(check))
+		    r(check);
+		if (check !== true) {
+		    var message	= "Failed at rule {0} with values {1}".format(rule, params);
+		    r({
+			"error": "Failed Validation",
+			"message": is_string(check) ? check : message
+		    })
+		}
+		f();
+	    }, params, self.args, self.db);
+	}));
     });
-    return true;
-}
-Endpoint.prototype.execute		= function(callback) {
-    var validations	= this.config.Get('.validate');
-    if (validations !== null) {
-	var score	= this.validate(validations);
-	if (score !== true)
-	    callback( score );
-    }
-
-    var method		= this.config.Get('.method');
-    if (method !== null) {
-	var cmd		= methodlib.Get(method);
-	if (cmd === null)
-	    throw new Error("No method named {0}".format(method));
-	else
-	    callback( cmd(this.args, this.db) );
-    }
-
-    var structure	= this.get_structure();
-    if (structure === false)
-	callback({
-	    "error": "Dead End",
-	    "message": "Nothing configured for this endpoint"
-	});
-
-    var query		= this.query();
-    this.db.all(query, function(err, all) {
-	var result	= structure === null
-	    ? all
-	    : restruct(all, structure);
-	callback(result);
-    });
-}
-Endpoint.prototype.get_structure	= function() {
-    var structure	= this.config.Get('.structure');
-    if (structure === null)
-	return false;
-
-    structure		= structure.copy();
-    var update		= this.config.Get('.structure_update');
-    if (update !== null) {
-	structure.update( update );
-	structure.iteritems(function(k,v) {
-	    if (v === false)
-		delete structure[k];
-	});
-    }
-    
-    return structure;
+    return Promise.all(promises);
 }
 Endpoint.prototype.query		= function() {
     this.table		= this.config.Get('.table');
@@ -248,6 +210,69 @@ Endpoint.prototype.query		= function() {
 
     query		= query.format(this.args);
     return query;
+}
+Endpoint.prototype.get_structure	= function() {
+    var structure	= this.config.Get('.structure');
+    if (structure === null)
+	return false;
+
+    structure		= structure.copy();
+    var update		= this.config.Get('.structure_update');
+    if (update !== null) {
+	structure.update( update );
+	structure.iteritems(function(k,v) {
+	    if (v === false)
+		delete structure[k];
+	});
+    }
+    
+    return structure;
+}
+Endpoint.prototype.execute		= function() {
+    var self		= this;
+    return new Promise(function(f,r) {
+	self.validate(self.config.Get('.validate'))
+	    .then(function() {
+		try {
+		    var method		= self.config.Get('.method');
+		    if (method !== null) {
+			var cmd		= methodlib.Get(method);
+			if (cmd === null)
+			    throw new Error("No method named {0}".format(method));
+			else
+			    return cmd.call(methodlib, function(result) {
+				f(result);
+			    }, self.args, self.db);
+		    }
+		    else {
+			var structure	= self.get_structure();
+			if (structure === false)
+			    return f({
+				"error": "Dead End",
+				"message": "Nothing configured for this endpoint"
+			    });
+
+			var query		= self.query();
+			self.db.all(query, function(err, all) {
+			    var result	= structure === null
+				? all
+				: restruct(all, structure);
+			    return f(result);
+			});
+			
+		    }
+		} catch (err) {
+		    r(err);
+		}
+	    }, function(error) {
+		if (isinstance(error, Error)) {
+		    r(error);
+		}
+		else {
+		    f(error);
+		}
+	    });
+    });
 }
 
 module.exports		= ChaosRouter;
